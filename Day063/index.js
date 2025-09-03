008b50047fc0a5865a17c4035f025aea52a1dd11
AnasZuberi26 <anas26092001@gmail.com>
2025-09-03 21:28:23 +0500

    3-Sep

---
+++ Day063/index.js
@@ -0,0 +1,56 @@
+let arr = [1,2,3,4,5,0,1,1200,12,121]
+
+// console.log(arr);
+// console.log(arr.length);
+// console.log(typeof arr);
+
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[4]);
+
+// Arrays are mutable but strings are immutable
+
+// arr[0] = 566;
+// console.log(arr);
+
+// let a = "Anas"
+// a[0] = 'K'
+// console.log(a[0]);
+// console.log(a);
+
+// console.log(arr.toString());
+// console.log(arr.join(" and "));
+
+// console.log(arr.pop());
+// console.log(arr);
+
+// console.log(arr.push(100));
+// console.log(arr);
+
+// console.log(arr.shift());
+// console.log(arr);
+
+// console.log(arr.unshift('0'));
+// console.log(arr);
+
+// console.log(delete arr[0]);
+// console.log(arr);
+
+// arr2 = [6,7,8]
+// arr3 = [9,10,11]
+
+// console.log(arr.concat(arr2));
+// console.log(arr.concat(arr2,arr3));
+
+// console.log(arr.sort());
+
+// console.log(arr.splice(2,1));
+// console.log(arr);
+// console.log(arr.splice(2,1,23,24));
+// console.log(arr);
+
+// console.log(arr.slice(2));
+// console.log(arr.slice(1,3));
+
+// console.log(arr.reverse());
+
