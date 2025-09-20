93b11ff3ad7a7887a07f70c886cfbdf313d6303e
AnasZuberi26 <anas26092001@gmail.com>
2025-09-20 22:39:33 +0500

    20-Sep

---
+++ Day083.2/Data Structures and Algorithm/004/app.js
@@ -0,0 +1,60 @@
+let prompt = require("prompt-sync")();
+// let a = prompt("Enter a number: ")
+
+// process.stdout.write(a)
+
+// --------------------------------Pattern Programming--------------------------------
+
+// console.log('*');
+// console.log('*');
+// console.log('*');
+// console.log('*');
+// console.log('*');
+// console.log('*');
+// console.log('*');
+
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+
+let n = prompt("Enter a Number: ");
+
+// for (let i = 1; i <= n; i++) {
+//   for (let i = 1; i <= n; i++) {
+//     process.stdout.write("*  ");
+//   }
+//   console.log("");
+// }
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i >= j) {
+//       process.stdout.write("*  ");
+//     }
+//   }
+//   console.log("");
+// }
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i >= j) {
+//       process.stdout.write(+j +"  ");
+//     }
+//   }
+//   console.log("");
+// }
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i; j++) {
+//     process.stdout.write("   ");
+//   }
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("*  ");
+//   }
+//   console.log("");
+// }
+
