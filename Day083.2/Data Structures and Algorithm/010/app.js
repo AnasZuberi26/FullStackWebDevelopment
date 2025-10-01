09800207aa74094f269543b4df2804c094909588
AnasZuberi26 <anas26092001@gmail.com>
2025-10-01 22:00:03 +0500

    31-Sep

---
+++ Day083.2/Data Structures and Algorithm/010/app.js
@@ -0,0 +1,61 @@
+// greet()
+
+// function greet(){
+//     console.log("Good Morning Pineapple");
+//     enjoy()
+// }
+
+// function enjoy(){
+//     console.log("Enjoy ho rha hai guys");
+//     temp()
+// }
+
+// function temp(){
+//     console.log("Kuch kar rhe ho");
+// }
+
+// -------------------------------Recursion-------------------------------
+
+// function temp(n){
+//     if(n==0){
+//         return;
+//     }
+//     console.log(n);
+//     temp(n-1)
+// }
+
+// temp(5)
+
+// -------------------------------Recursion-------------------------------
+
+// function temp(n) {
+//   if (n == 0) {
+//     return;
+//   }
+//   temp(n - 1);
+//   console.log(n);
+// }
+
+// temp(5);
+
+// -------------------------------Sum to the nth number-------------------------------
+
+// function calSum(n) {
+//   if (n == 1) {
+//     return 1;
+//   }
+//   return n + calSum(n - 1);
+// }
+
+// console.log("sum is: " + calSum(7));
+
+// -------------------------------Factorial-------------------------------
+
+function factorial(n) {
+  if (n == 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log("Factorial is: " + factorial(5));
