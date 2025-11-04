d815cf7a602a61391bb39a6b86612e63242aadeb
AnasZuberi26 <anas26092001@gmail.com>
2025-11-04 19:29:15 +0500

    4-Nov

--- Day086/main.js
+++ Day086/main.js
@@ -1,14 +1,33 @@
-const { createServer } = require('node:http');
+// const { createServer } = require("node:http");
+// const fs = require("fs");
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// import { createServer } from "http";
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const hostname = "127.0.0.1";
+// const port = 3000;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/html");
+//   res.end("<h1>Hello World</h1>");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// --------------------Module--------------------
+
+// import { a, b, c, d, e } from "./myModule.js";
+// console.log(a, b, c, d, e);
+
+// import obj from "./myModule.js";
+// console.log(obj);
+
+// import myObj from "./myModule.js";
+// console.log(myObj);
+
+// --------------------Commom-JS--------------------
+
+const a = require("./myModule2.js");
+console.log(a, __dirname, __filename);
