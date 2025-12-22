18811dc3815cead6b15a882a481e52a5cac631ec
AnasZuberi26 <anas26092001@gmail.com>
2025-12-22 20:57:01 +0500

    22-Dec

---
+++ Day121/first/app/about/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Problems solved by Next.js</h1>
+      <ul>
+        <li>Full Stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Optimized Redering</li>
+        <li>Additional Features like Router from Next/Navigation</li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
